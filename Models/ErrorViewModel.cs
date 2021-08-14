9eb4f0dc956369a9a2ad461ee084444882f79b66
shashankjaincloud <shashankjaincloud@outlook.com>
2021-08-15 00:15:12 +0530

    initinialize

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cicddemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
